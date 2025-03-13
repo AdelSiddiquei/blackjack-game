ff42ca09ccb883e6ae652910ec82246fc062284b
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-03-13 17:41:41 +0000

    Formatted files with ruff.

--- blackjack.py
+++ blackjack.py
@@ -1,12 +1,13 @@
 import random as rand
 
 cards = []
-suits = ['spades', 'hearts', 'clubs', 'diamonds']
-ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+suits = ["spades", "hearts", "clubs", "diamonds"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 for suit in suits:
     for rank in ranks:
         cards.append([suit, rank])
+
 
 def shuffle() -> list:
     """
@@ -16,13 +17,14 @@ def shuffle() -> list:
 
 
 def deal() -> list:
-    '''This will deal a single card from a full, shuffled deck.
+    """This will deal a single card from a full, shuffled deck.
 
     Returns:
         a single card from our deck
-    '''
+    """
     hand = cards.pop()
     return hand
+
 
 shuffle()
 hand = deal()
