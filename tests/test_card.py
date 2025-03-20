9dd5d2ffbacad8557065449e1b247d4418236735
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-03-20 17:33:12 +0000

    turned test_card into a function

--- tests/test_card.py
+++ tests/test_card.py
@@ -2,7 +2,8 @@ from blackjack import blackjack as bj
 
 card1 = bj.Card("hearts", {"rank": "esbs", "value": 2})
 
-try:
-    print(card1)
-except Exception as e:
-    print(f"An error occured {e}")
+def test_str():
+    try:
+        print(card1)
+    except Exception as e:
+        print(f"An error occured {e}")
